d33024bf7d308320ed803850dc788bd09f8580f1
Ivan Starčević <ivan.starcevic15@gmail.com>
2024-09-11 11:00:48 +0200

    add scroll to top

--- src/router/index.ts
+++ src/router/index.ts
@@ -64,7 +64,14 @@ const router = createRouter({
       name: 'not-found',
       component: NotFound
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router
